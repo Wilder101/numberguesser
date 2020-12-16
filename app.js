d67030699906325b47d0c04aee8298d062866845
GitWilder <43479032+Wilder101@users.noreply.github.com>
2020-12-15 16:40:51 -0800

    Lose case and game over

--- app.js
+++ app.js
@@ -35,18 +35,53 @@ guessBtn.addEventListener("click", function(){
 
     // Check if won
     if(guess === winningNum){
+        // Game over - won
 
-        // Disable input
-        guessInput.disabled = true;
+        // // Disable input
+        // guessInput.disabled = true;
 
-        // Change border color
-        guessInput.style.borderColor = "green";
+        // // Change border color
+        // guessInput.style.borderColor = "green";
 
-        // Set message
-        setMessage(`${winningNum} is correct. YOU WIN!`, "green");
+        // // Set message
+        // setMessage(`${winningNum} is correct. YOU WIN!`, "green");
+
+        gameOver(true, `${winningNum} is correct. YOU WIN!`);
 
     } else {
 
+        // Wrong number
+        guessLeft -= 1;
+
+        // Check if any guesses remain
+        if(guessLeft === 0){
+
+            // Game over - lost
+
+            // // Disable input
+            // guessInput.disabled = true;
+
+            // // Change border color
+            // guessInput.style.borderColor = "red";
+
+            // // Set message
+            // setMessage(`Game Over. You lost. The correct number was ${winningNum}`, "red");
+
+            gameOver(false, `Game Over. You lost. The correct number was ${winningNum}`)
+
+        } else {
+
+            // Game continues - answer wrong
+
+            // Change border color
+            guessInput.style.borderColor = "red";
+
+            // Clear input
+            guessInput.value = "";
+
+            // Inform user of wrong number
+            setMessage(`${guess} is not correct. ${guessLeft} guesses left`, "red");
+        }
     }
 });
 
@@ -54,5 +89,23 @@ guessBtn.addEventListener("click", function(){
 function setMessage(msg, color){
     message.style.color = color;
     message.textContent = msg;
+}
 
+// Game over
+function gameOver(won, msg){
+
+    let color;
+    won === true ? color = "green" : color = "red";
+
+    // Disable input
+    guessInput.disabled = true;
+
+    // Change border color
+    guessInput.style.borderColor = color;
+
+    // Set text color
+    message.style.color = color;
+
+    // Set message
+    setMessage(msg, color);
 }
